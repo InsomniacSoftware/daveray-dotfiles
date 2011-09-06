177e49151152bb56f116d010e7e426a529631f9b
Dave Ray <daveray@gmail.com>
2011-09-06 14:30:05 -0400

    hgrc and hg prompt

--- bashrc.sh
+++ bashrc.sh
@@ -5,8 +5,10 @@ yellow='\033[1;33m'
 green='\033[1;32m'
 red='\033[1;31m'
 blue='\033[1;34m'
+cyan='\033[1;36m'
 fgcolor="\033[0m" # unsets color to term's fg color
 
 gitprompt='$(__git_ps1 "[%s]")'
+hgprompt='`hg prompt "[{branch}{status}{update}]" 2> /dev/null`'
 
-export PS1="\[$yellow\][\!,\$?] \[$blue\]\W\[$green\] $gitprompt\[$fgcolor\]\$ "
+export PS1="\[$yellow\][\!,\$?] \[$blue\]\W \[$cyan\]${hgprompt}\[$green\]${gitprompt}\[$fgcolor\]\$ "
--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ echo -e "\nsource $DOTFILES/bashrc.sh\n" >> $TARGET/.bashrc
 ln -s $DOTFILES/gitconfig $TARGET/.gitconfig
 ln -s $DOTFILES/gitignore $TARGET/.gitignore
 
+# Hg junk
+echo -e "\n%include $DOTFILES/hgrc.ini\n" >> $TARGET/.hgrc
+
 # Vim junk.
 git clone git@github.com:daveray/vimfiles.git $TARGET/.vim
 echo -e "runtime vimrc.vim\n" > $TARGET/.vimrc
