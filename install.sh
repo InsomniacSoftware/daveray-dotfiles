250abe6bd44c9aa70582e7c41a89579c1fd58e0b
Dave Ray <daveray@gmail.com>
2011-09-07 21:08:35 -0400

    updates for osx install

--- install.sh
+++ install.sh
@@ -14,5 +14,7 @@ echo -e "\n%include $DOTFILES/hgrc.ini\n" >> $TARGET/.hgrc
 # Vim junk.
 git clone git@github.com:daveray/vimfiles.git $TARGET/.vim
 echo -e "runtime vimrc.vim\n" > $TARGET/.vimrc
+make -C $TARGET/.vim/bundle/vimclojure-2.2.0/lib/nailgun
 mkdir $TARGET/vimbak
+
 
