63dc46e2e765fb7284c9ed6b2ad263a71347d2a3
Dave Ray <daveray@gmail.com>
2011-09-08 13:02:41 -0400

    Set up .gvimrc

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ echo -e "\n%include $DOTFILES/hgrc.ini\n" >> $TARGET/.hgrc
 # Vim junk.
 git clone git@github.com:daveray/vimfiles.git $TARGET/.vim
 echo -e "runtime vimrc.vim\n" > $TARGET/.vimrc
+echo -e "runtime gvimrc.vim\n" > $TARGET/.gvimrc
 make -C $TARGET/.vim/bundle/vimclojure-2.2.0/lib/nailgun
 mkdir $TARGET/vimbak
 
