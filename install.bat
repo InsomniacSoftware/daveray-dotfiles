5e976dfdd65a440dfd5410389455d8c6253d4b1c
Dave Ray <daveray@gmail.com>
2011-11-17 10:13:31 -0500

    windows.

---
+++ install.bat
@@ -0,0 +1,4 @@
+REM Despite trying this (http://superuser.com/questions/124679/how-do-i-create-an-mklink-in-windows-7-home-premium-as-a-regular-user),
+REM I couldn't get mklink to work. Stupid.
+copy vimperatorrc.vim "%HOME%\.vimperatorrc"
+
