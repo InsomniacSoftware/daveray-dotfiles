ae88b2399359a398cdfc2a856a61a4c50c8c6895
Dave Ray <daveray@gmail.com>
2011-12-09 09:09:02 -0500

    fnction to set terminal title

--- bashrc.sh
+++ bashrc.sh
@@ -18,3 +18,18 @@ fgcolor="\033[0m" # unsets color to term's fg color
 gitprompt='$(__git_ps1 "[%s]")'
 hgprompt='`hg prompt "[{branch}{status}{update}]" 2> /dev/null`'
 export PS1="\[$yellow\][\!,\$?] \[$blue\]\W \[$cyan\]${hgprompt}\[$green\]${gitprompt}\[$fgcolor\]\$ "
+
+# Set terminal title
+# @param string $1  Tab/window title
+# @param string $2  (optional) Separate window title
+# The latest version of this software can be obtained here:
+# See: http://fvue.nl/wiki/NameTerminal
+function title() {
+    [ "${TERM:0:5}" = "xterm" ]   && local ansiNrTab=0
+    [ "$TERM"       = "rxvt" ]    && local ansiNrTab=61
+    [ "$TERM"       = "konsole" ] && local ansiNrTab=30 ansiNrWindow=0
+    # Change tab title
+    [ $ansiNrTab ] && echo -n $'\e'"]$ansiNrTab;$1"$'\a'
+    # If terminal support separate window title, change window title as well
+    [ $ansiNrWindow -a "$2" ] && echo -n $'\e'"]$ansiNrWindow;$2"$'\a'
+}
