6bba12eda870c4937af22377dd8ad0a4166d13ab
Dave Ray <daveray@gmail.com>
2012-12-14 14:42:57 -0800

    script to make a thread dump

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user
- {:plugins  [[lein-tarsier "0.9.1"]]
-            :vimclojure-opts {:repl true}}}
+ {:plugins  [[lein-tarsier "0.9.4"]
+             [lein-ring "0.7.5"] ]
+  :vimclojure-opts {:repl true}}}
