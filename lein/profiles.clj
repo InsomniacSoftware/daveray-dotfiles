344606198ec90c7135999387ab053598083412e3
Dave Ray <daveray@gmail.com>
2013-12-05 22:07:14 -0800

    Cleanup lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user
- {:plugins  [[lein-tarsier "0.9.4"]
-             [lein-ring "0.7.5"]
-             [lein-clojars "0.9.1"]]
-  :vimclojure-opts {:repl true}}}
+ {
+  :dependencies [] 
+  :injections []  ; redl
+  :plugins  []
+  }}
