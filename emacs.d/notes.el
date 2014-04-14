93a3b755769388dac92e8df7c78e740281f046fe
Dave Ray <daveray@gmail.com>
2014-04-14 12:25:40 -0700

    next/previous log day

--- emacs.d/notes.el
+++ emacs.d/notes.el
@@ -12,6 +12,30 @@
   (interactive)
   (split-window-below 20)
   (find-file (format-time-string "~/.notes/log/%Y-%m-%d.txt")))
+
+(defun notes-parse-time-from-name (name)
+  (when (string-match "^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9].txt$" name)
+    (let* ((time (parse-time-string (file-name-sans-extension name))))
+      (setf (nth 0 time) 0)
+      (setf (nth 1 time) 0)
+      (setf (nth 2 time) 0)
+      time)))
+
+(defun notes-next-log-name* (d-days)
+  (let* ((name (buffer-name (current-buffer)))
+         (next-time (notes-parse-time-from-name name)))
+    (when next-time
+      (setf (nth 0 next-time) 0)
+      (setf (nth 1 next-time) 0)
+      (setf (nth 2 next-time) 0)
+      (setf (nth 3 next-time) (+ (nth 3 next-time) d-days))
+      (format-time-string "~/.notes/log/%Y-%m-%d.txt" (apply 'encode-time next-time)))))
+
+(defun notes-next-log (d-days)
+  (let* ((name (notes-next-log-name* d-days)))
+    (when name
+      (find-file name))))
+
 (defun notes-log-timestamp ()
   (interactive)
   (insert (format-time-string "%Y-%m-%d %a %I:%M %p\n")))
@@ -22,3 +46,6 @@
 (define-key notes-map (kbd "n f") 'notes-grep)
 (define-key notes-map (kbd "n l") 'notes-log)
 (define-key notes-map (kbd "n t") 'notes-log-timestamp)
+(define-key notes-map (kbd "n =") (lambda () (interactive) (notes-next-log 1)))  ; next day
+(define-key notes-map (kbd "n -") (lambda () (interactive) (notes-next-log -1))) ; previous day
+
